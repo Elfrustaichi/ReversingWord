16516b7c1f31472d081a49146083aed3b5e759fd
Umid Museyibli <118839456+Elfrustaichi@users.noreply.github.com>
2022-12-14 15:51:06 +0400

    Add files via upload

---
+++ ReversingWord/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+string Word = "Hello World";
+string ReversedWord = string.Empty;
+string[] SplittedWord = Word.Split(' ');
+string result = string.Empty;
+result = $"{Reversing(SplittedWord[0])} {Reversing(SplittedWord[1])}";
+
+Console.WriteLine(result);
+
+
+static string Reversing(string word)
+{
+    string ReversedWord = string.Empty;
+    
+    for (int i = word.Length - 1; i >= 0; i--)
+    {
+        ReversedWord += word[i];
+    }
+    return ReversedWord;
+}
+
+
